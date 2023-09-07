2dbf8dc170ad08a677e897c077db0d8c83259985
alondramora <mora_ac@yahoo.com>
2023-09-06 20:39:06 -0700

    added an h1 with the title of wellness checklist

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="inputItem">New Item</label>
-        <input type="text" id="inputItem" />
-      </div>
-      <button className="btn">Add</button>
-    </form>
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="inputItem">New Item</label>
+          <input type="text" id="inputItem" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1>Wellness Checklist</h1>
+    </>
   );
 }
