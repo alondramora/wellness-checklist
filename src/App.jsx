b1a8d54b25304259e2765a6059bb946174bab441
alondramora <mora_ac@yahoo.com>
2023-09-08 21:21:21 -0700

    added an onChange event listener to the input inside of the rendered lis. When we check a box aka make a change, the toggleTodo handler function will take the id and the checked properties and it will change that to do items checked property to true

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,22 @@ export default function App() {
         { id: crypto.randomUUID(), title: newItem, completed: false },
       ];
     });
+
+    setNewItem("");
   }
 
-  console.log(todos);
+  // console.log(todos);
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
 
   return (
     <>
@@ -34,20 +47,21 @@ export default function App() {
       </form>
       <h1>Wellness Checklist</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Study React
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 2
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </label>
+              <button className="btn btn-danger">Delete</button>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
